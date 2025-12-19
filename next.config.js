84adb64d27a38cb1a1cf066bbcb18c513d688a0d
Atul Rana <atulrana2668@gmail.com>
2025-12-19 23:08:53 +0530

    Add Vercel deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: false,
+    domains: [],
+    remotePatterns: [],
+  },
+  // Vercel will automatically handle these optimizations
+  swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
 }
 
 module.exports = nextConfig
